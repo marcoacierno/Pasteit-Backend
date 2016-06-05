f2f21114ba91e9b8248821d3f2f8a9c968e2d9ff
Marco Acierno <marcoacierno96@gmail.com>
2016-06-05 14:44:47 +0200

    Add 'paste' node to query a single paste

--- pastes/schema.py
+++ pastes/schema.py
@@ -30,6 +30,18 @@ class PasteNode(DjangoNode):
 
 class Query(graphene.ObjectType):
     pastes = DjangoFilterConnectionField(PasteNode)
+    paste = graphene.Field(PasteNode, hash_id=graphene.String())
+
+    def resolve_paste(self, args, info):
+        hash_id = args.get('hash_id', None)
+
+        if not hash_id:
+            return None
+
+        try:
+            return Paste.objects.get(hash_id=hash_id)
+        except Paste.DoesNotExist:
+            return None
 
     def resolve_pastes(self, args, info):
         return Paste.objects.filter(visibility=Paste.VISIBILITY[0][0]).all()
